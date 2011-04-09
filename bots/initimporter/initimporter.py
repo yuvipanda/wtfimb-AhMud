548364f42e31a81db88ab1fb5700d89d410d8b63
J Kishore kumar <jkk@fullerene>
2011-04-10 01:48:26 +0530

    Initimporter now imports stops to mediawiki

--- bots/initimporter/initimporter.py
+++ bots/initimporter/initimporter.py
@@ -1,11 +1,31 @@
 import csv
 from jinja2 import Template
+import mwclient
+import sys
+
+try:
+    import settings
+    site = mwclient.Site(settings.host_name, settings.site_path)
+    site.login(settings.username, settings.password)
+except ImportError:
+    print "Cannot find settings.py"
+    sys.exit(-1)
 
 routes_reader = csv.DictReader(open("routes.csv"))
 routes = dict([(r["name"],[]) for r in routes_reader])
 
 stops_reader = csv.DictReader(open("stops.csv"))
-stops = dict([(r["name"], r) for r in stops_reader])
+
+stops_unsanitized = dict([(r["name"], r) for r in stops_reader])
+stops = {}
+
+def sanitize(ip):
+    return ip.replace(";","")
+
+for sname in stops_unsanitized:
+    sanitized_name = sanitize(sname)
+    stops[sanitized_name] = stops_unsanitized[sname]
+    stops[sanitized_name]["name"] = sanitized_name
 
 route_stops_reader = csv.DictReader(open("routestops.csv"))
 for rs in route_stops_reader:
@@ -23,7 +43,9 @@ for r in routes:
 
 for s in stops:
     stop = stop_template.render(stop=stops[s])
-    print stop
-    raw_input()
-
-print routes
+    page = site.pages[settings.city_prefix + stops[s]["name"]]
+    text = page.edit()
+    if text == "":
+        page.save(stop, summary="Import from old bri data")
+    else:
+        print "stop '%s' exists already. Not overwriting" % stops[s]["name"]
---
+++ bots/initimporter/settings.sample.py
@@ -0,0 +1,11 @@
+# Copy to logindetails.py and change values
+
+host_name = 'localhost'
+site_path = '/wiki/'
+
+# Wikibot login credentials
+username = 'importbot'
+password = ''
+
+city_prefix = 'Chennai/'
+
